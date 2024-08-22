4a1e5781102ea33aaf159c5acce25c17f069327c
shil-mil <shyshy122303@gmail.com>
2024-08-22 15:41:51 +0800

    Add file contents to create products and view products

---
+++ admin/products.php
@@ -0,0 +1,32 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Menu Products
+                <a href="products-create.php" class = "btn btn-primary float-end">Add Menu Product</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
