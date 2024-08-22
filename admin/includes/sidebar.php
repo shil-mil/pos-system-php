9cd0f68c2503d335bafc65d92990e40cbba2e917
shil-mil <shyshy122303@gmail.com>
2024-08-22 15:27:07 +0800

    Add add admin and view admins in sidebar

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -50,15 +50,20 @@
                         </div>
                     </nav>
                 </div>
-                <div class="sb-sidenav-menu-heading">Addons</div>
-                <a class="nav-link" href="charts.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                    Charts
+                <div class="sb-sidenav-menu-heading">Manage Users</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAdmins" aria-expanded="false" aria-controls="collapseAdmins">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Admins/Staff
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <a class="nav-link" href="tables.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Tables
-                </a>
+                <div class="collapse" id="collapseAdmins" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="admins-create.php">Add Admin</a>
+                        <a class="nav-link" href="admins.php">View Admins</a>
+                    </nav>
+                </div>
+
+
             </div>
         </div>
         <div class="sb-sidenav-footer">
