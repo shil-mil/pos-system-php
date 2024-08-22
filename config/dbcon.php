e38b464c4663e6eaf24eebb3d8c4d3e30663c996
shil-mil <shyshy122303@gmail.com>
2024-08-22 18:56:20 +0800

    Added missing semi-colons

--- config/dbcon.php
+++ config/dbcon.php
@@ -3,12 +3,12 @@
 define('DB_SERVER', "localhost");
 define('DB_USERNAME', "root");
 define ('DB_PASSWORD',"");
-define('DB_DATABASE',"db_kapitansisig")
+define('DB_DATABASE',"db_KapitanSisig");
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if(!$conn){
-    die("Connection Failed: ". mysqli_connect_error())
+    die("Connection Failed: ". mysqli_connect_error());
 }
 
 ?>
