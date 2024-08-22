637ec6b2970d18e66d46852b28ed2fcb7127cf3c
shil-mil <shyshy122303@gmail.com>
2024-08-22 19:09:15 +0800

    Removed unnecessary spaces

--- config/dbcon.php
+++ config/dbcon.php
@@ -2,8 +2,8 @@
 
 define('DB_SERVER', "localhost");
 define('DB_USERNAME', "root");
-define ('DB_PASSWORD',"");
-define('DB_DATABASE',"db_KapitanSisig");
+define('DB_PASSWORD', "");
+define('DB_DATABASE',"db_kapitanSisig");
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
