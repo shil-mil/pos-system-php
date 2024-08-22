1520b2900e493f9358616df0bd610083cfc147cd
shil-mil <shyshy122303@gmail.com>
2024-08-22 15:36:52 +0800

    Added add new product and view products options

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -63,6 +63,18 @@
                     </nav>
                 </div>
 
+                <div class="sb-sidenav-menu-heading">Manage Menu Products</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProducts" aria-expanded="false" aria-controls="collapseProducts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Menu Products
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseProducts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="products-create.php">Add Menu Product</a>
+                        <a class="nav-link" href="products.php">View Menu Products</a>
+                    </nav>
+                </div>
 
             </div>
         </div>
