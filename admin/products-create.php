1ea30cb0f4a46295d704cf16224d03c17d121c69
shil-mil <shyshy122303@gmail.com>
2024-08-22 17:54:53 +0800

    Added form for product details

--- admin/products-create.php
+++ admin/products-create.php
@@ -8,7 +8,29 @@
             </h4>
         </div>
         <div class="card-body">
-            
+        <form action="code.php" method="POST">
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="">Name *</label>
+                        <input type="text" name="name" required class="form-control" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Price *</label>
+                        <input type="number" name="price" required class="form-control" />
+                    </div>
+                    <!-- <div class="col-md-6 mb-3">
+                        <label for="">Password *</label>
+                        <input type="password" name="password" required class="form-control" />
+                    </div> FOR PIC NA NI SUNOD -->
+                    <!-- <div class="col-md-6 mb-3">
+                        <label for="">Phone Number *</label>
+                        <input type="number" name="phone" required class="form-control" />
+                    </div> FOR CATEGORY??? GOTTA ASK FIRST!!   -->
+                    <div class="col-md-12 mb-3 text-end">
+                        <button type="submit" name="saveAdmin" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
