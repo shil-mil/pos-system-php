865cd59ed227ec470e3db69963eef4c005913370
shil-mil <shyshy122303@gmail.com>
2024-08-22 17:03:23 +0800

    Add form for new admin details

--- admin/admins-create.php
+++ admin/admins-create.php
@@ -8,7 +8,33 @@
             </h4>
         </div>
         <div class="card-body">
-            
+            <form action="code.php" method="POST">
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="">Name *</label>
+                        <input type="text" name="name" required class="form-control" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Email *</label>
+                        <input type="email" name="email" required class="form-control" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Password *</label>
+                        <input type="password" name="password" required class="form-control" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Phone Number *</label>
+                        <input type="number" name="phone" required class="form-control" />
+                    </div>   
+                    <div class="col-md-3 mb-3">
+                        <label for="">Is Banned</label>
+                        <input type="checkbox" name="is_banned" required style="width:30px;height:30px;" />
+                    </div>
+                    <div class="col-md-12 mb-3 text-end">
+                        <button type="submit" name="saveAdmin" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
