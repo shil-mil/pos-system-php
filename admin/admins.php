0f502a3bc6f85233e53d46e48c24330aeac60060
shil-mil <shyshy122303@gmail.com>
2024-08-22 15:12:42 +0800

    Add admis.php file content

---
+++ admin/admins.php
@@ -0,0 +1,32 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card">
+        <div class="card-header">
+            <h4 class="mb-0">Admins/Staff
+                <a href="admins-create.php" class = "btn btn-primary float-end">Admin</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">.table
+                <div class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
