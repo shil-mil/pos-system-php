0623361c9b021e12b75a6707fdf718612927ab63
shil-mil <shyshy122303@gmail.com>
2024-08-22 18:27:32 +0800

    Modify form details to Kapitan Sisig

--- admin/admins-create.php
+++ admin/admins-create.php
@@ -11,20 +11,28 @@
             <form action="code.php" method="POST">
                 <div class="row">
                     <div class="col-md-12 mb-3">
-                        <label for="">Name *</label>
-                        <input type="text" name="name" required class="form-control" />
+                        <label for="">First Name *</label>
+                        <input type="text" name="firstname" required class="form-control" />
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Last Name *</label>
+                        <input type="text" name="lastname" required class="form-control" />
                     </div>
                     <div class="col-md-6 mb-3">
-                        <label for="">Email *</label>
-                        <input type="email" name="email" required class="form-control" />
+                        <label for="">Username *</label>
+                        <input type="text" name="username" required class="form-control" />
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="">Password *</label>
                         <input type="password" name="password" required class="form-control" />
                     </div>
-                    <div class="col-md-6 mb-3">
+                    <!-- <div class="col-md-6 mb-3">
                         <label for="">Phone Number *</label>
                         <input type="number" name="phone" required class="form-control" />
+                    </div>    -->
+                    <div class="col-md-6 mb-3">
+                        <label for="">Position *</label>
+                        <input type="text" name="position" required class="form-control" />
                     </div>   
                     <div class="col-md-3 mb-3">
                         <label for="">Is Banned</label>
