67a2bc2654e720ab9e07fb5264b03581fd616ef5
shil-mil <shyshy122303@gmail.com>
2024-08-22 14:08:09 +0800

    Added functions validate, redirect, alertMessage, insert, update, getAll, getById, delete

---
+++ config/function.php
@@ -0,0 +1,136 @@
+<?php
+session_start();
+
+require'dbcon.php';
+
+
+// Input field validation
+function validate(){
+
+    global $conn;
+    $validatedData = mysqli_real_escape_string($conn, $inputDate);
+    return trim($validatedData);
+}
+
+// Redirect from one page to another with status message
+
+function redirect($url, $status){
+
+    $_SESSION['status'] = $status;
+    eader('Location: '.$url);
+    exit(0);
+}
+
+// Display messages or status after any process
+
+function alertMessage(){
+
+    if(isset($_SESSION['status'])){
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <h3>'.$_SESSION['status'].'</h3>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+        unset($_SESSION['status']);
+    }
+}
+
+// Insert record
+function insert($tableName, $data){
+    global $conn;
+
+    $table = validate($tableName);
+
+    $columns = array_keys($data);
+    $values = array_values($data);
+
+    $finalColumn = implode(',', $columns);
+    $finalValues = "'".implode("', '", $values)."'";
+
+    $query = "INSERT INTO $table ($finalColumn) VALUES ($finalValues)";
+    $result = mysqli_query($conn,$query);
+    return $result;
+}
+
+// Update data
+function update($tableName, $id, $data) {
+
+    global $conn;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $updateDataSrting = "";
+
+    foreach($data as $column => $value) {
+        $updateDataSrting .= $column.'='."'$value',";
+    }
+
+    $finalUpdateData = substr(trim($updateDataSrting),0,-1);
+
+    $query = "UPDATE $table SET $finalUpdateData WHERE id='$id'";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
+
+function getAll($tableName, $status = NULL){
+    global $conn;
+
+    $table = validate($tableName);
+    $status = validate($status);
+
+    if($status == 'status'){
+        $query = "SELECT * FROM $table WHERE status='0'";
+    } else {
+        $query = "SELECT * FROM $table";
+    }
+    return mysqli_query($conn, $query);
+}
+
+function getById($tableName, $id){
+    global $conn;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $query = "SELECT * FROM $table WHERE id='$id' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+
+    if($result) {
+        if(mysqli_num_rows($result) == 1){
+            $row = mysqli_fetch_assoc($result);
+            $response = [
+                'status' => 404,
+                'data' => $row,
+                'message' => 'Record found'
+            ];
+            return $response;
+        } else {
+            $response = [
+                'status' => 404,
+                'message' => 'No data found'
+            ];
+            return $response;
+        }
+    } else {
+        $response = [
+            'status' => 500,
+            'message' => 'Something went wrong.'
+        ];
+        return $response;
+    }
+}
+
+// Delete data from database using id
+function delete($tableName, $id){
+
+    global $conn;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $query = "DELETE FROM $table WHERE id='$id' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
+
+?>
