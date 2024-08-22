a12dc408c4f30487e82df087634ce926f8c9649c
shil-mil <shyshy122303@gmail.com>
2024-08-22 09:34:45 +0800

    Add Kapitan Sisig Logo and login button

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php include('includes/header.php'); ?>
-    <h1>Hello, world!</h1>
+    <div class="py-5">
+        <div class="row">
+            <div class="col-md-12">
+            <img src="img/Kapitan-Sisig-Logo.png" class="rounded-circle mx-auto d-block" alt="Kapitan Sisig Logo">
+                <a href="login.php" class="btn btn-primary mt-4">Login</a>
+            </div>
+        </div>
+    </div>
 <?php include('includes/footer.php'); ?>
