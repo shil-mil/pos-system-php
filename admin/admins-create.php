b2aec83c9daa4c3cea49c9b0cdcc8b11f649bdc9
shil-mil <shyshy122303@gmail.com>
2024-08-22 15:19:13 +0800

    Add admins-create.php file content

---
+++ admin/admins-create.php
@@ -0,0 +1,16 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card">
+        <div class="card-header">
+            <h4 class="mb-0">Add Admin
+                <a href="admins-create.php" class = "btn btn-primary float-end">Back</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
