78dadc6e88b63a89e8878180745044b9c5f95bc7
shil-mil <shyshy122303@gmail.com>
2024-08-23 10:40:52 +0800

    Add details to display database data

--- admin/admins.php
+++ admin/admins.php
@@ -9,23 +9,51 @@
         </div>
         <div class="card-body">
         <?php  alertMessage(); ?>
+        <?php 
+            $admins = getAll('admins'); 
+            if(!$admins) {
+                 echo '<h4>Something went wrong.</h4>';
+                 return false;
+            }
+            if(mysqli_num_rows($admins)>0) {
+        ?>
             <div class="table-responsive">
                 <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Position</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
+                        
+                        <?php foreach($admins as $adminItem) : ?>
                         <tr>
-                            <td></td>
+                            <td><?= $adminItem['id'] ?></td>
+                            <td><?= $adminItem['username'] ?></td>
+                            <td><?= $adminItem['firstname'] ?></td>
+                            <td><?= $adminItem['lastname'] ?></td>
+                            <td><?= $adminItem['position'] ?></td>
+                            <td>
+                                <a href="admins-edit.php" class="btn btn-success btn-sm">Edit</a>
+                                <a href="admins-delete.php" class="btn btn-danger btn-sm">Delete</a>
+                            </td>
                         </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
+            <?php 
+                } else {
+                    ?>
+                        <h4 class="mb-0">No record found.</h4>
+                    <?php
+                } 
+                ?>
         </div>
     </div>
 </div>
