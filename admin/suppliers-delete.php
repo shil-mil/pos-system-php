c4bb6a88fc33a068916cb62030662e1e2aa6db4f
shil-mil <shyshy122303@gmail.com>
2024-08-23 13:24:59 +0800

    Add file to delete suppliers

---
+++ admin/suppliers-delete.php
@@ -0,0 +1,25 @@
+<?php 
+
+require '../config/function.php';
+
+$paramResultId = checkParamId('id');
+if(is_numeric($paramResultId)) {
+    $supplierId = validate($paramResultId);
+
+    $supplier = getById('suppliers', $supplierId);
+
+    if($supplier['status'] == 200){
+        $supplierDeleteRes = delete('suppliers', $supplierId);
+        if($supplierDeleteRes) {
+            redirect('suppliers.php','Supplier deleted successfully!');
+        } else {
+            redirect('suppliers.php','Something went wrong.');
+        }
+    } else {
+        redirect('suppliers.php',$supplier['message']);
+    }
+} else {
+    redirect('suppliers.php','Something went wrong.');
+}
+
+?>
