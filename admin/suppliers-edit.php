5a3d2e38c599293644fe9b38b00e4f0ecbf21974
shil-mil <shyshy122303@gmail.com>
2024-08-23 12:48:02 +0800

    Add form to update supplier details

---
+++ admin/suppliers-edit.php
@@ -0,0 +1,71 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Edit Supplier
+                <a href="suppliers.php" class = "btn btn-danger float-end">Back</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+        <?php alertMessage();?>
+            <form action="code.php" method="POST">
+
+            <?php 
+                if(isset($_GET['id'])) {
+                    if($_GET['id'] != '') {
+                        $supplierId = $_GET['id'];
+                    } else {
+                        echo '<h5>No ID found.</h5>';
+                        return false;
+                    }
+                } else {
+                    echo '<h5>No ID given in parameters.</h5>';
+                    return false;
+                }
+
+                $supplierData = getById('suppliers', $supplierId);
+                if($supplierData){
+                    if($supplierData['status'] = 200) {
+                        ?>
+                        <input type="hidden" name="supplierId" value="<?= $supplierData['data']['id']; ?>" >
+
+                        <div class="row">
+                            <div class="col-md-12 mb-3">
+                                <label for="">First Name *</label>
+                                <input type="text" name="firstname" required value="<?= $supplierData['data']['firstname']; ?>" class="form-control" />
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="">Last Name *</label>
+                                <input type="text" name="lastname" required value="<?= $supplierData['data']['lastname']; ?>" class="form-control" />
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="">Phone Number *</label>
+                                <input type="text" name="phonenumber" required value="<?= $supplierData['data']['phonenumber']; ?>" class="form-control" />
+                            </div>   
+                            <div class="col-md-12 mb-3">
+                                <label for="">Address *</label>
+                                <input type="text" name="address" value="<?= $supplierData['data']['address']; ?>" required class="form-control" />
+                            </div> 
+                            <div class="col-md-12 mb-3 text-end">
+                                <button type="submit" name="updateSupplier" class="btn btn-primary">Update</button>
+                            </div>
+                        </div>
+                        <?php
+                    } else {
+                        echo '<h5'.$supplierData>['message'].'</h5>';
+                    }
+                } else {
+                    echo 'Something went wrong.';
+                    return false;
+
+                }
+            ?>
+
+                
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
