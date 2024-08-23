872f4be4c1db8ee76deb133f3e608027019fe39d
shil-mil <shyshy122303@gmail.com>
2024-08-23 18:14:00 +0800

    Delete category code

---
+++ admin/categories-delete.php
@@ -0,0 +1,25 @@
+<?php 
+
+require '../config/function.php';
+
+$paramResultId = checkParamId('id');
+if(is_numeric($paramResultId)) {
+    $categoryId = validate($paramResultId);
+
+    $category = getById('categories', $categoryId);
+
+    if($category['status'] == 200){
+        $categoryDeleteRes = delete('categories', $categoryId);
+        if($categoryDeleteRes) {
+            redirect('categories.php','Category deleted successfully!');
+        } else {
+            redirect('categories.php','Something went wrong.');
+        }
+    } else {
+        redirect('categories.php',$category['message']);
+    }
+} else {
+    redirect('categories.php','Something went wrong.');
+}
+
+?>
