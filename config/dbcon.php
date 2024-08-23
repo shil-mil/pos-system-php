254cf320b5278cbcd6d55c1d348c587c5e2fd8c3
shil-mil <shyshy122303@gmail.com>
2024-08-23 10:13:33 +0800

    Add port number to end of

--- config/dbcon.php
+++ config/dbcon.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', "localhost");
 define('DB_USERNAME', "root");
 define('DB_PASSWORD', "");
-define('DB_DATABASE',"db_kapitansisig");
+define('DB_DATABASE',"db_KapitanSisig");
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, 4306);
 
