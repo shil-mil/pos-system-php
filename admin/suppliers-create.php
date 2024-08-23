2f9975f75e81da4cac3aeacfd73157b06191b39f
shil-mil <shyshy122303@gmail.com>
2024-08-23 11:22:26 +0800

    Add form details to create supplier

---
+++ admin/suppliers-create.php
@@ -0,0 +1,39 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Add Supplier
+                <a href="suppliers.php" class = "btn btn-primary float-end">Back</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+        <?php alertMessage();?>
+            <form action="code.php" method="POST">
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="">First Name *</label>
+                        <input type="text" name="firstname" required class="form-control" />
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Last Name *</label>
+                        <input type="text" name="lastname" required class="form-control" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Phone Number *</label>
+                        <input type="text" name="phonenumber" required class="form-control" />
+                    </div>   
+                    <div class="col-md-12 mb-3">
+                        <label for="">Address *</label>
+                        <input type="text" name="address" required class="form-control" />
+                    </div>   
+                    <div class="col-md-12 mb-3 text-end">
+                        <button type="submit" name="saveSupplier" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
