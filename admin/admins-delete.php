f5f3a2a34aabfc8b3f10eda777cf6712587e6ad3
shil-mil <shyshy122303@gmail.com>
2024-08-23 13:01:53 +0800

    Add code to delete admin

---
+++ admin/admins-delete.php
@@ -0,0 +1,25 @@
+<?php 
+
+require '../config/function.php';
+
+$paramResultId = checkParamId('id');
+if(is_numeric($paramResultId)) {
+    $adminId = validate($paramResultId);
+
+    $admin = getById('admins', $adminId);
+
+    if($admin['status'] == 200){
+        $adminDeleteRes = delete('admins', $adminId);
+        if($adminDeleteRes) {
+            redirect('admins.php','Admin deleted successfully!');
+        } else {
+            redirect('admins.php','Something went wrong.');
+        }
+    } else {
+        redirect('admins.php',$admin['message']);
+    }
+} else {
+    redirect('admins.php','Something went wrong.');
+}
+
+?>
