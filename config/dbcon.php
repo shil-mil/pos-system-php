9192ead44acdeefd6f33d441a2f780de23246750
shil-mil <shyshy122303@gmail.com>
2024-08-23 09:26:33 +0800

    Add 4306 to

--- config/dbcon.php
+++ config/dbcon.php
@@ -3,9 +3,9 @@
 define('DB_SERVER', "localhost");
 define('DB_USERNAME', "root");
 define('DB_PASSWORD', "");
-define('DB_DATABASE',"db_kapitanSisig");
+define('DB_DATABASE',"db_kapitansisig");
 
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, 4306);
 
 if(!$conn){
     die("Connection Failed: ". mysqli_connect_error());
