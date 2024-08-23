d85d7b909240cab3ceb3706a49aac5492819e337
shil-mil <shyshy122303@gmail.com>
2024-08-23 13:00:19 +0800

    Change href of delete button

--- admin/admins.php
+++ admin/admins.php
@@ -40,7 +40,7 @@
                             <td><?= $adminItem['position'] ?></td>
                             <td>
                                 <a href="admins-edit.php?id=<?= $adminItem['id'];?>" class="btn btn-success btn-sm">Edit</a>
-                                <a href="admins-delete.php" class="btn btn-danger btn-sm">Delete</a>
+                                <a href="admins-delete.php?id=<?= $adminItem['id'];?>" class="btn btn-danger btn-sm">Delete</a>
                             </td>
                         </tr>
                         <?php endforeach; ?>
