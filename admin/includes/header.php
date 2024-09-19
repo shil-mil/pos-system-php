a5b99b3a5a849276f03306286efe10205ecc2d6b
shil-mil <shyshy122303@gmail.com>
2024-09-19 19:35:48 +0800

    Added alertify lines

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,7 +1,8 @@
 <?php
 require '../config/function.php';
-require 'authentication.php';
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,18 +14,25 @@ require 'authentication.php';
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
         <link href="assets/css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/default.min.css"/>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css"/>
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="assets/css/custom.css">
+        
+  
+
+  
     </head>
     <body class="sb-nav-fixed">
 
-      <?php include('navbar.php'); ?>
-
+    <?php include('navbar.php'); ?>
       <div id="layoutSidenav">
-
-        <?php include('sidebar.php'); ?>
+        <?php include('sidebar.php'); ?>  
 
           <div id="layoutSidenav_content">
 
