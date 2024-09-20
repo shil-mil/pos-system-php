e998edc10dbee195b9ba4761f5b8291d0397a9f1
shil-mil <shyshy122303@gmail.com>
2024-09-21 00:28:54 +0800

    Added code for viewing specific order

---
+++ admin/orders-view.php
@@ -0,0 +1,176 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Order View
+                <a href="orders.php" class="button btn-danger mx-2 btn-sm float-end">Back</a>
+            </h4>
+        </div>
+        <div class="card-body">
+
+        <?php alertMessage();?>
+
+        <?php
+
+        if(isset($_GET['track'])){
+
+            if($_GET['track']== ''){
+                ?>
+                <div class="text-center py-5">
+                    <h5>No tracking number found!</h5>
+                    <div>
+                    <a href="orders.php" class="btn btn-primary mt-4 w-25">Return to Orders View</a>
+                    </div>
+                    
+                    </div>
+                <?php
+
+                return false;
+            }
+            $trackingNo = validate($_GET['track']);
+
+            $query = "SELECT o.*, c.* FROM orders o, customers c WHERE 
+                        c.id = o.customer_id AND tracking_no='$trackingNo' 
+                        ORDER BY o.id DESC";
+
+            $orders = mysqli_query($conn, $query);
+
+            if($orders){
+
+                if(mysqli_num_rows($orders) > 0){
+                    $orderData = mysqli_fetch_assoc($orders);
+                    $orderId = $orderData['id'];
+
+                    ?>
+                    <div class="card card-body shadow border-1 mb-4 ">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h4>Order Details</h4>
+                                <label class="mb-1">
+                                    Tracking No: <span class="fw-bold"><?= htmlspecialchars($orderData['tracking_no'], ENT_QUOTES, 'UTF-8'); ?></span>
+                                </label>
+                                <br/>
+                                <label class="mb-1">
+                                    Order Date: <span class="fw-bold"><?= htmlspecialchars($orderData['order_date'], ENT_QUOTES, 'UTF-8'); ?></span>
+                                </label>
+                                <br/>
+                                <label class="mb-1">
+                                    Order Status: <span class="fw-bold"><?= htmlspecialchars($orderData['order_status'], ENT_QUOTES, 'UTF-8'); ?></span>
+                                </label>
+                                <br/>
+                                <label class="mb-1">
+                                    Payment Method: <span class="fw-bold"><?= htmlspecialchars($orderData['payment_mode'], ENT_QUOTES, 'UTF-8'); ?></span>
+                                </label>
+                                <br/>
+                            </div>
+
+                            <div class="col-md-6">
+                                <h4>User Details</h4>
+
+                                <label class="mb-1">
+                                    Full Name: <span class="fw-bold"><?= htmlspecialchars($orderData['name'], ENT_QUOTES, 'UTF-8'); ?></span>
+                                </label>
+                                <br/>
+                                <label class="mb-1">
+                                    Phone Number: <span class="fw-bold"><?= htmlspecialchars($orderData['phone'], ENT_QUOTES, 'UTF-8'); ?></span>
+                                </label>
+                                <br/>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <?php
+                        $orderItemQuery = "SELECT oi.quantity as orderItemQuantity, oi.price as orderItemPrice, o.*, oi.*, p.* 
+                                FROM orders as o, order_items as oi, products as p 
+                                WHERE  oi.order_id = o.id AND p.id = oi.product_id AND o.tracking_no='$trackingNo' ";
+
+                        $orderItemsRes = mysqli_query($conn, $orderItemQuery);
+
+                        if($orderItemsRes){
+                            if(mysqli_num_rows($orderItemsRes) > 0){
+                                $totalAmount = 0;
+                                ?>
+                                    <h4 class="my-3">Order Items Details</h4>
+                                    <table class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Product</th>
+                                                
+                                                <th>Price</th>
+                                                <th>Quantity</th>
+                                                <th>Total</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php while($orderItemRow = mysqli_fetch_assoc($orderItemsRes)): ?>
+                                                <tr>
+                                                    <td>
+                                                    <img src="<?= $orderItemRow['image'] != '' ? '../'.$orderItemRow['image'] : '../pics/uploads/no-img.jpg'; ?>"
+
+                                                        style="width:50px;height:50px;"
+                                                        alt="Img"/>
+
+                                                        <?= $orderItemRow['productname'];?>
+                                                    </td>
+                                                    
+                                                    <td width="15%" class="fw-bold text-center">
+                                                        <?= number_format($orderItemRow['orderItemPrice'], 0); ?>
+                                                    </td>
+                                                    <td width="15%" class="fw-bold text-center">
+                                                        <?= $orderItemRow['orderItemQuantity']; ?>
+                                                    </td>
+                                                    <td width="15%" class="fw-bold text-center">
+                                                        <?= number_format($orderItemRow['orderItemPrice'] * $orderItemRow['orderItemQuantity'], 0); ?>
+                                                    </td> 
+                                                </tr>
+                                                <?php $totalAmount += $orderItemRow['orderItemPrice'] * $orderItemRow['orderItemQuantity']; ?>
+                                            <?php endwhile; ?>
+
+                                            <tr>
+                                                <td class="text-end fw-bold">Total Price: </td>
+                                                <td colspan="3" class="text-end fw-bold">Rs: <?= number_format($totalAmount, 0); ?></td>
+                                            </tr>
+                                        </tbody>
+
+                                    </table>
+                                <?php
+                            }else{
+                                echo ' <h5>No record Found!</h5>';
+                                return false;
+                            }
+                        }else{
+                            echo ' <h5>No record Found!</h5>';
+                            return false;
+                        }
+                    ?>
+                    <?php
+                }else{
+                    echo ' <h5>No record Found!</h5>';
+                    return false;
+                }
+
+            }else{
+                echo ' <h5>Something Went Wrong!</h5>';
+            }
+        }else{
+            ?>
+                <div class="text-center py-5">
+                    <h5>No tracking number found!</h5>
+                    <div>
+                    <a href="orders.php" class="btn btn-primary mt-4 w-25">Return to Orders View</a>
+                    </div>
+                    
+                </div>
+            <?php
+        }
+        ?>
+
+       
+
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
