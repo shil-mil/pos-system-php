42ed86f085cd4ac5f5a2ff268cd973a7eb9cd843
shil-mil <shyshy122303@gmail.com>
2024-09-21 00:14:12 +0800

    Changed href link of dashboard from index.html to index.php

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -4,7 +4,7 @@
             <div class="nav">
                 <!-- Main Section -->
                 <div class="sb-sidenav-menu-heading">Main</div>
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
