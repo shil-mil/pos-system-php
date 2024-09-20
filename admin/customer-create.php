27b540053d10611050e87e02fd1a66c2cdcab23d
shil-mil <shyshy122303@gmail.com>
2024-09-20 13:19:11 +0800

    Added code for creating customer

---
+++ admin/customer-create.php
@@ -0,0 +1,34 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Add Customer
+                <a href="categories.php" class = "btn btn-outline-danger float-end">Back</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+        <?php alertMessage();?>
+            <form action="code.php" method="POST">
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="">Name *</label>
+                        <input type="text" name="name" required class="form-control" />
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Phone Number *</label>
+                        <input type="number" name="phone" required class="form-control" />
+                    </div>
+                    
+                   
+                    <div class="col-md-6 mb-3 text-end">
+                        <br />
+                        <button type="submit" name="saveCustomer" class="btn btn-outline-primary">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
