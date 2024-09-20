5641abf63dd448f37df5530b2f9b029da7d95c08
shil-mil <shyshy122303@gmail.com>
2024-09-20 13:17:47 +0800

    Added section to manage customers

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,22 +1,23 @@
 <div id="layoutSidenav_nav">
-   <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <div class="sb-sidenav-menu-heading">Core</div>
+                <!-- Main Section -->
+                <div class="sb-sidenav-menu-heading">Main</div>
                 <a class="nav-link" href="index.html">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
-
                 <a class="nav-link" href="order-create.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-bell"></i></div>
                     Create Order
                 </a>
-
                 <a class="nav-link" href="orders.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-list"></i></div>
                     Orders
                 </a>
+
+                <!-- Interface Section -->
                 <div class="sb-sidenav-menu-heading">Interface</div>
 
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCategory" aria-expanded="false" aria-controls="collapseCategory">
@@ -25,55 +26,70 @@
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse" id="collapseCategory" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="categories.php">View Categories</a>    
-                        <a class="nav-link" href="categories-create.php">Create Category</a>
+                    <nav class="sb-sidenav-menu-nested nav">   
+                        <a class="nav-link" href="categories-create.php">Add Category</a>
+                        <a class="nav-link" href="categories.php">View Categories</a> 
                     </nav>
                 </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Pages
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                            Authentication
+
+                <!-- Units of Measurement -->
+                <li class="nav-item">
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUnits" aria-expanded="false" aria-controls="collapseUnits">
+                            <i class="fas fa-balance-scale"></i>
+                            <span>Units of Measurement</span>
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                        <div class="collapse" id="collapseUnits" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="login.html">Login</a>
-                                <a class="nav-link" href="register.html">Register</a>
-                                <a class="nav-link" href="password.html">Forgot Password</a>
+                                <a class="nav-link" href="units-create.php">Add Unit</a>
+                                <a class="nav-link" href="units.php">View Units</a>
                             </nav>
                         </div>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                            Error
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="401.html">401 Page</a>
-                                <a class="nav-link" href="404.html">404 Page</a>
-                                <a class="nav-link" href="500.html">500 Page</a>
-                            </nav>
-                        </div>
-                    </nav>
-                </div>
-                <div class="sb-sidenav-menu-heading">Manage Users</div>
+                </li>
+
+            
+
+                <!-- Manage Staff Section -->
+                <div class="sb-sidenav-menu-heading">Manage Staff</div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAdmins" aria-expanded="false" aria-controls="collapseAdmins">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                     Admins/Staff
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse" id="collapseAdmins" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="admins.php">View Admins</a>
                         <a class="nav-link" href="admins-create.php">Add Admin</a>
+                        <a class="nav-link" href="admins.php">View Admin</a>
                     </nav>
                 </div>
 
+                <!-- Manage Customer Section -->
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCustomer" aria-expanded="false" aria-controls="collapseCustomer">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Customer
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseCustomer" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="customers-create.php">Add Customer</a>
+                        <a class="nav-link" href="customers.php">View Customer</a>
+                    </nav>
+                </div>
+
+                <!-- Suppliers Section -->
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseSuppliers" aria-expanded="false" aria-controls="collapseSuppliers">
+                    <div class="sb-nav-link-icon"><i class="fas fa-truck"></i></div>
+                    Suppliers
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseSuppliers" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="suppliers-create.php">Add Suppliers</a>
+                        <a class="nav-link" href="suppliers.php">View Suppliers</a>
+                    </nav>
+                </div>
+
+                <!-- Products Section -->
                 <div class="sb-sidenav-menu-heading">Manage Menu Products</div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProducts" aria-expanded="false" aria-controls="collapseProducts">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
@@ -87,16 +103,16 @@
                     </nav>
                 </div>
 
-                <div class="sb-sidenav-menu-heading">Manage Suppliers</div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseSuppliers" aria-expanded="false" aria-controls="collapseProducts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                    Suppliers
+                <!-- Ingredients Section -->
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseIngredients" aria-expanded="false" aria-controls="collapseIngredients">
+                    <div class="sb-nav-link-icon"><i class="fas fa-utensils"></i></div>
+                    Ingredients
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapseSuppliers" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse" id="collapseIngredients" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="suppliers.php">View Suppliers</a>
-                        <a class="nav-link" href="suppliers-create.php">Add Supplier</a>
+                        <a class="nav-link" href="ingredients-add.php">Add Ingredients</a>
+                        <a class="nav-link" href="ingredients-view.php">View Ingredients</a>
                     </nav>
                 </div>
 
