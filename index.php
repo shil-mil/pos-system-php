0bccff14a39ae1bda5f26a7ef25edcc0e9b67095
shil-mil <shyshy122303@gmail.com>
2024-10-05 21:30:44 +0800

    Made button larger, put it in centered div

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
         <div class="row">
             <div class="col-md-12">
             <img src="img/Kapitan-Sisig-Logo.png" class="rounded-circle mx-auto d-block" alt="Kapitan Sisig Logo">
-                <a href="login.php" class="btn btn-primary mt-4">Log In</a>
+                <div class="text-center">
+                    <a href="login.php" class="btn btn-primary btn-lg mt-4">Log In</a>
+                </div>
             </div>
         </div>
     </div>
