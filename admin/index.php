2aa926e8b158a2aad4911b87c16b40d2b0d92f7b
shil-mil <shyshy122303@gmail.com>
2024-10-06 22:37:17 +0800

    Added alertMessage() and add margin top to container

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php include('includes/header.php'); ?>
 
-<div class="container-fluid px-4">
+<div class="container-fluid px-4 mt-4">
+    <?php alertMessage();?>
     <h1 class="mt-4">Dashboard</h1>
     <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item active">Dashboard</li>
