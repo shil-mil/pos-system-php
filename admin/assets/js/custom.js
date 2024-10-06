be09e49c97242b7936b05357a91363ce5b85b135
shil-mil <shyshy122303@gmail.com>
2024-10-06 09:56:28 +0800

    Added functions for printMyBillingArea and downloadPDF

--- admin/assets/js/custom.js
+++ admin/assets/js/custom.js
@@ -191,7 +191,43 @@ $(document).ready(function(){
         });
     });    
 
-
+    function printMyBillingArea(){
+        var divContents = document.getElementById("myBillingArea").innerHTML;
+        var a = window.open('', '');
+        a.document.write('<html><title>Kapitan Sisig</title>');
+        a.document.write('<body style="font-family: fangsong;">');
+        a.document.write(divContents);
+        a.document.write('</body></html>');
+        a.document.close;
+        a.print();
+    }
 
     
 });
+
+function printMyBillingArea(){
+    var divContents = document.getElementById("myBillingArea").innerHTML;
+    var a = window.open('', '');
+    a.document.write('<html><title>Kapitan Sisig</title>');
+    a.document.write('<body style="font-family: fangsong;">');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close;
+    a.print();
+}
+
+window.jsPDF = window.jspdf.jsPDF;
+var docPDF = new jsPDF();
+
+function downloadPDF(invoiceNo){
+    var elementHTML = document.querySelector("#myBillingArea");
+    docPDF.html( elementHTML, {
+        callback: function() {
+            docPDF.save(invoiceNo+'.pdf');
+        },
+        x: 15,
+        y: 15,
+        width: 170,
+        windowWidth: 650
+    });
+}
