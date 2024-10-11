0541be23c8cfe9e63e885ea6a1a805387c64a0cf
shil-mil <shyshy122303@gmail.com>
2024-10-11 20:21:19 +0800

    Added code for units

---
+++ admin/units-create.php
@@ -0,0 +1,33 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fluid px-4">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Add Units
+                <a href="units.php" class = "btn btn-outline-danger float-end">Back</a> 
+            </h4>
+        </div>
+        <div class="card-body">
+        <?php alertMessage();?>
+            <form action="code.php" method="POST">
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="">Name *</label>
+                        <input type="text" name="name" required class="form-control" />
+                    </div>
+                    <div class="col-md-6">
+                        <label for="">Status (Unchecked=Visible, Checked=Hidden")</label>
+                        <br />
+                        <input type="checkbox" style="width:30px;height:30px;" name="status">
+                    </div>
+                    <div class="col-md-6 mb-3 text-end">
+                        <br />
+                        <button type="submit" name="saveUnit" class="btn btn-outline-primary">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
