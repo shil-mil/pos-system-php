2c13fa0aa1bda2f4fe63b6c374d13308c85f95a7
shil-mil <shyshy122303@gmail.com>
2024-10-13 20:33:03 +0800

    Changed default to Placed, added order_status 'Placed'

--- admin/purchase-orders.php
+++ admin/purchase-orders.php
@@ -15,12 +15,16 @@
                 if(mysqli_num_rows($purchaseOrders) > 0){
 
                     // Prepare arrays to hold orders by their status
+                    $placedPurchaseOrders = [];
                     $pendingPurchaseOrders = [];
                     $deliveredPurchaseOrders = [];
 
                     // Categorize orders based on their status
                     foreach($purchaseOrders as $orderItem) {
                         switch ($orderItem['order_status']) {
+                            case 'Placed':
+                                $placedPurchaseOrders[] = $orderItem;
+                                break;
                             case 'Pending':
                                 $pendingPurchaseOrders[] = $orderItem;
                                 break;
@@ -31,6 +35,37 @@
                     }
                     ?>
                     <!-- Pending Purchase Orders -->
+                    <h5>Placed Purchase Orders</h5>
+                    <table class="table table-striped table-bordered align-items-center justify-content-center">
+                        <thead>
+                            <tr>
+                                <th>Tracking No.</th>
+                                <th>Name</th>
+                                <th>Order Date</th>
+                                <th>Order Status</th>
+                                <th>Payment Method</th>
+                                <th style="width: 250px;">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($placedPurchaseOrders as $ingredientItem): ?>
+                                <tr>
+                                    <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
+                                    <td><?= $ingredientItem['firstname']; ?></td>
+                                    <td><?= $ingredientItem['order_date']; ?></td>
+                                    <td><?= $ingredientItem['order_status']; ?></td>
+                                    <td><?= $ingredientItem['ingPayment_mode']; ?></td>
+                                    <td style="display: flex; justify-content: space-evenly; gap: 5px;">
+                                        <input type="hidden" name="order_track" value="<?= $ingredientItem['tracking_no']; ?>">
+                                        <a href="order-edit.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-primary btn-sm w-100">Edit</a>
+                                        <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+
+                    <!-- Placed Purchase Orders -->
                     <h5>Pending Purchase Orders</h5>
                     <table class="table table-striped table-bordered align-items-center justify-content-center">
                         <thead>
