1921c5a13927da91028a205eabdba8d60900701e
shil-mil <shyshy122303@gmail.com>
2024-10-26 19:00:27 +0800

    Changed view to tab panes

--- admin/purchase-orders.php
+++ admin/purchase-orders.php
@@ -38,125 +38,152 @@
                         }
                     }
                     ?>
-                    <!-- Pending Purchase Orders -->
-                    <h5>Placed Purchase Orders</h5>
-                    <table class="table table-striped table-bordered align-items-center justify-content-center">
-                        <thead>
-                            <tr>
-                                <th>Tracking No.</th>
-                                <th>Name</th>
-                                <th>Order Date</th>
-                                <th>Order Status</th>
-                                <th>Payment Method</th>
-                                <th style="width: 250px;">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($placedPurchaseOrders as $ingredientItem): ?>
-                                <tr>
-                                    <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
-                                    <td><?= $ingredientItem['firstname']; ?></td>
-                                    <td><?= $ingredientItem['order_date']; ?></td>
-                                    <td><?= $ingredientItem['order_status']; ?></td>
-                                    <td><?= $ingredientItem['ingPayment_mode']; ?></td>
-                                    <td style="display: flex; justify-content: space-evenly; gap: 5px;">
-                                        <input type="hidden" name="order_track" value="<?= $ingredientItem['tracking_no']; ?>">
-                                        <a href="purchase-order-edit.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-primary btn-sm w-100">Edit</a>
-                                        <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
 
-                    <!-- Placed Purchase Orders -->
-                    <h5>Pending Purchase Orders</h5>
-                    <table class="table table-striped table-bordered align-items-center justify-content-center">
-                        <thead>
-                            <tr>
-                                <th>Tracking No.</th>
-                                <th>Name</th>
-                                <th>Order Date</th>
-                                <th>Order Status</th>
-                                <th>Payment Method</th>
-                                <th style="width: 250px;">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($pendingPurchaseOrders as $ingredientItem): ?>
+                     <!-- Nav Tabs -->
+                     <ul class="nav nav-tabs" id="purchaseOrderTabs" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link active" id="placed-purchase-orders-tab" data-bs-toggle="tab" href="#placedPurchaseOrders" role="tab" aria-controls="placedPurchaseOrders" aria-selected="true">Placed</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="pending-purchase-orders-tab" data-bs-toggle="tab" href="#pendingPurchaseOrders" role="tab" aria-controls="pendingPurchaseOrders" aria-selected="false">Pending</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="delivered-purchase-orders-tab" data-bs-toggle="tab" href="#deliveredPurchaseOrders" role="tab" aria-controls="deliveredPurchaseOrders" aria-selected="false">Delivered</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="cancelled-purchase-orders-tab" data-bs-toggle="tab" href="#cancelledPurchaseOrders" role="tab" aria-controls="cancelledPurchaseOrders" aria-selected="false">Cancelled</a>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content" id="purchaseOrderTabsContent">
+                        <!-- Placed Purchase Orders -->
+                        <div class="tab-pane mt-3 fade show active" id="placedPurchaseOrders" role="tabpanel" aria-labelledby="placed-purchase-orders-tab">
+                            <h5>Placed Purchase Orders</h5>
+                            <table class="table table-striped table-bordered align-items-center justify-content-center">
+                                <thead>
+                                    <tr>
+                                        <th>Purchase Order No.</th>
+                                        <th>Name</th>
+                                        <th>Order Date</th>
+                                        <th>Order Status</th>
+                                        <th>Payment Method</th>
+                                        <th style="width: 250px;">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($placedPurchaseOrders as $ingredientItem): ?>
+                                        <tr>
+                                            <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
+                                            <td><?= $ingredientItem['firstname']; ?></td>
+                                            <td><?= $ingredientItem['order_date']; ?></td>
+                                            <td><?= $ingredientItem['order_status']; ?></td>
+                                            <td><?= $ingredientItem['ingPayment_mode']; ?></td>
+                                            <td style="display: flex; justify-content: space-evenly; gap: 5px;">
+                                                <input type="hidden" name="order_track" value="<?= $ingredientItem['tracking_no']; ?>">
+                                                <a href="purchase-order-edit.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-primary btn-sm w-100">Edit</a>
+                                                <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    <!-- Pending Purchase Orders -->
+                    <div class="tab-pane mt-3 fade" id="pendingPurchaseOrders" role="tabpanel" aria-labelledby="pending-purchase-orders-tab">
+                        <h5>Pending Purchase Orders</h5>
+                        <table class="table table-striped table-bordered align-items-center justify-content-center">
+                            <thead>
                                 <tr>
-                                    <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
-                                    <td><?= $ingredientItem['firstname']; ?></td>
-                                    <td><?= $ingredientItem['order_date']; ?></td>
-                                    <td><?= $ingredientItem['order_status']; ?></td>
-                                    <td><?= $ingredientItem['ingPayment_mode']; ?></td>
-                                    <td style="display: flex; justify-content: space-evenly; gap: 5px;">
-                                        <input type="hidden" name="order_track" value="<?= $ingredientItem['tracking_no']; ?>">
-                                        <button class="btn btn-warning btn-sm complete-btn w-100 proceedToDeliveredIng">Delivered</button>
-                                        <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
-                                    </td>
+                                    <th>Purchase Order No.</th>
+                                    <th>Name</th>
+                                    <th>Order Date</th>
+                                    <th>Order Status</th>
+                                    <th>Payment Method</th>
+                                    <th style="width: 250px;">Action</th>
                                 </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach($pendingPurchaseOrders as $ingredientItem): ?>
+                                    <tr>
+                                        <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
+                                        <td><?= $ingredientItem['firstname']; ?></td>
+                                        <td><?= $ingredientItem['order_date']; ?></td>
+                                        <td><?= $ingredientItem['order_status']; ?></td>
+                                        <td><?= $ingredientItem['ingPayment_mode']; ?></td>
+                                        <td style="display: flex; justify-content: space-evenly; gap: 5px;">
+                                            <input type="hidden" name="order_track" value="<?= $ingredientItem['tracking_no']; ?>">
+                                            <button class="btn btn-warning btn-sm complete-btn w-100 proceedToDeliveredIng">Delivered</button>
+                                            <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
 
                     <!-- Delivered Purchase Orders -->
-                    <h5>Delivered Purchase Orders</h5>
-                    <table class="table table-striped table-bordered align-items-center justify-content-center">
-                        <thead>
-                            <tr>
-                                <th>Tracking No.</th>
-                                <th>Name</th>
-                                <th>Order Date</th>
-                                <th>Order Status</th>
-                                <th>Payment Method</th>
-                                <th style="width: 250px;">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($deliveredPurchaseOrders as $ingredientItem): ?>
+                    <div class="tab-pane mt-3 fade" id="deliveredPurchaseOrders" role="tabpanel" aria-labelledby="delivered-purchase-orders-tab">
+                        <h5>Delivered Purchase Orders</h5>
+                        <table class="table table-striped table-bordered align-items-center justify-content-center">
+                            <thead>
                                 <tr>
-                                    <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
-                                    <td><?= $ingredientItem['firstname']; ?></td>
-                                    <td><?= $ingredientItem['order_date']; ?></td>
-                                    <td><?= $ingredientItem['order_status']; ?></td>
-                                    <td><?= $ingredientItem['ingPayment_mode']; ?></td>
-                                    <td>
-                                        <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
-                                    </td>
+                                    <th>Purchase Order No.</th>
+                                    <th>Name</th>
+                                    <th>Order Date</th>
+                                    <th>Order Status</th>
+                                    <th>Payment Method</th>
+                                    <th style="width: 250px;">Action</th>
                                 </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach($deliveredPurchaseOrders as $ingredientItem): ?>
+                                    <tr>
+                                        <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
+                                        <td><?= $ingredientItem['firstname']; ?></td>
+                                        <td><?= $ingredientItem['order_date']; ?></td>
+                                        <td><?= $ingredientItem['order_status']; ?></td>
+                                        <td><?= $ingredientItem['ingPayment_mode']; ?></td>
+                                        <td>
+                                            <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
                     
                     <!-- Cancelled Purchase Orders -->
-                    <h5>Cancelled Purchase Orders</h5>
-                    <table class="table table-striped table-bordered align-items-center justify-content-center">
-                        <thead>
-                            <tr>
-                                <th>Tracking No.</th>
-                                <th>Name</th>
-                                <th>Order Date</th>
-                                <th>Order Status</th>
-                                <th>Payment Method</th>
-                                <th style="width: 250px;">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($cancelledPurchaseOrders as $ingredientItem): ?>
+                    <div class="tab-pane mt-3 fade" id="cancelledPurchaseOrders" role="tabpanel" aria-labelledby="cancelled-purchase-orders-tab">
+                        <h5>Cancelled Purchase Orders</h5>
+                        <table class="table table-striped table-bordered align-items-center justify-content-center">
+                            <thead>
                                 <tr>
-                                    <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
-                                    <td><?= $ingredientItem['firstname']; ?></td>
-                                    <td><?= $ingredientItem['order_date']; ?></td>
-                                    <td><?= $ingredientItem['order_status']; ?></td>
-                                    <td><?= $ingredientItem['ingPayment_mode']; ?></td>
-                                    <td>
-                                        <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
-                                    </td>
+                                    <th>Purchase Order No.</th>
+                                    <th>Name</th>
+                                    <th>Order Date</th>
+                                    <th>Order Status</th>
+                                    <th>Payment Method</th>
+                                    <th style="width: 250px;">Action</th>
                                 </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach($cancelledPurchaseOrders as $ingredientItem): ?>
+                                    <tr>
+                                        <td class="fw-bold"><?= $ingredientItem['tracking_no']; ?></td>
+                                        <td><?= $ingredientItem['firstname']; ?></td>
+                                        <td><?= $ingredientItem['order_date']; ?></td>
+                                        <td><?= $ingredientItem['order_status']; ?></td>
+                                        <td><?= $ingredientItem['ingPayment_mode']; ?></td>
+                                        <td>
+                                            <a href="purchase-orders-view.php?track=<?= $ingredientItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm w-100">View</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    </div>
                     <?php
                 } else {
                     echo "<h5>No Record Available</h5>";
