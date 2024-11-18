f079fe18cfd9a5136a14d410863010f4114cdb26
shil-mil <101624541+shil-mil@users.noreply.github.com>
2024-11-18 23:46:19 +0800

    Update db_kapitansisig (1).sql
    Added expiryDate to stockin_ingredients
    Changed int to decimal several
    Removed Price from Ingredients table

--- db_kapitansisig (1).sql
+++ db_kapitansisig (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:4306
--- Generation Time: Nov 12, 2024 at 03:34 AM
+-- Generation Time: Nov 18, 2024 at 04:44 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -89,7 +89,8 @@ INSERT INTO `customers` (`id`, `name`) VALUES
 (3, 'Shiloh Millondaga'),
 (4, 'Evanica'),
 (5, 'Kristyle'),
-(6, 'Shiloh');
+(6, 'Shiloh'),
+(7, 'AJ');
 
 -- --------------------------------------------------------
 
@@ -102,20 +103,20 @@ CREATE TABLE `ingredients` (
   `name` varchar(191) NOT NULL,
   `unit_id` int(11) NOT NULL,
   `category` varchar(191) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `quantity` decimal(10,2) NOT NULL
+  `quantity` decimal(10,2) NOT NULL,
+  `reorder_point` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ingredients`
 --
 
-INSERT INTO `ingredients` (`id`, `name`, `unit_id`, `category`, `price`, `quantity`) VALUES
-(4, 'Pork Belly', 13, 'Meat & Poultry', 0.00, 15500.00),
-(5, 'Spoon', 14, 'Cutlery', 0.00, 1010.00),
-(6, 'Soy Sauce', 18, 'Condiments', 0.00, 13450.00),
-(7, 'Tuna', 13, 'Meat & Poultry', 0.00, 900.00),
-(8, 'Egg', 14, 'Others', 0.00, 1107.00);
+INSERT INTO `ingredients` (`id`, `name`, `unit_id`, `category`, `quantity`, `reorder_point`) VALUES
+(4, 'Pork Belly', 13, 'Meat & Poultry', 3000.00, 10.00),
+(5, 'Spoon', 14, 'Cutlery', 4.00, 10.00),
+(6, 'Soy Sauce', 18, 'Condiments', 6000.00, 10.00),
+(7, 'Tuna', 13, 'Meat & Poultry', 0.00, 10.00),
+(8, 'Egg', 14, 'Others', 43.00, 10.00);
 
 -- --------------------------------------------------------
 
@@ -129,7 +130,7 @@ CREATE TABLE `ingredients_items` (
   `ingredient_id` int(11) NOT NULL,
   `unit_id` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -137,46 +138,14 @@ CREATE TABLE `ingredients_items` (
 --
 
 INSERT INTO `ingredients_items` (`id`, `order_id`, `ingredient_id`, `unit_id`, `price`, `quantity`) VALUES
-(1, 1, 15, 0, 0.00, 1),
-(2, 2, 15, 0, 0.00, 5),
-(9, 9, 8, 0, 0.00, 1),
-(10, 10, 5, 0, 150.00, 4),
-(11, 11, 1, 0, 120.00, 1),
-(12, 12, 5, 0, 150.00, 3),
-(13, 13, 5, 0, 150.00, 6),
-(14, 14, 5, 0, 150.00, 1),
-(15, 15, 5, 0, 150.00, 1),
-(16, 16, 5, 16, 150.00, 1),
-(17, 17, 4, 16, 150.00, 2),
-(18, 17, 7, 16, 170.00, 1),
-(19, 17, 8, 15, 120.00, 1),
-(20, 18, 6, 19, 70.00, 1),
-(21, 19, 4, 16, 250.00, 1),
-(22, 20, 5, 14, 10.00, 1),
-(23, 20, 6, 19, 70.00, 2),
-(24, 20, 4, 16, 250.00, 2),
-(25, 21, 4, 16, 150.00, 4),
-(26, 21, 7, 16, 170.00, 2),
-(27, 22, 5, 14, 10.00, 1),
-(28, 22, 6, 19, 70.00, 1),
-(29, 22, 4, 16, 250.00, 1),
-(30, 23, 5, 14, 10.00, 1),
-(31, 24, 6, 19, 70.00, 2),
-(32, 24, 4, 16, 250.00, 1),
-(33, 25, 4, 16, 250.00, 1),
-(34, 26, 4, 16, 150.00, 1),
-(35, 27, 5, 14, 10.00, 4),
-(36, 27, 6, 19, 70.00, 1),
-(37, 27, 4, 16, 250.00, 1),
-(38, 27, 8, 15, 120.00, 1),
-(39, 28, 5, 14, 10.00, 1),
-(40, 28, 6, 19, 70.00, 1),
-(41, 28, 8, 15, 120.00, 1),
-(42, 28, 4, 16, 250.00, 1),
-(43, 29, 5, 14, 10.00, 1),
-(44, 29, 6, 19, 70.00, 1),
-(45, 29, 4, 16, 250.00, 1),
-(46, 29, 8, 15, 120.00, 1);
+(67, 41, 5, 14, 10.00, 1.00),
+(68, 41, 6, 19, 70.00, 1.00),
+(69, 41, 4, 16, 250.00, 1.00),
+(70, 41, 8, 15, 120.00, 1.00),
+(71, 42, 5, 14, 10.00, 2.00),
+(72, 42, 6, 19, 70.00, 4.00),
+(73, 42, 4, 16, 250.00, 2.00),
+(74, 42, 8, 15, 120.00, 3.00);
 
 -- --------------------------------------------------------
 
@@ -190,6 +159,8 @@ CREATE TABLE `orders` (
   `tracking_no` varchar(100) NOT NULL,
   `invoice_no` varchar(100) NOT NULL,
   `total_amount` varchar(100) NOT NULL,
+  `amount_received` decimal(10,2) NOT NULL,
+  `change_money` varchar(200) NOT NULL,
   `order_date` datetime DEFAULT NULL,
   `order_status` enum('Placed','Preparing','Completed','Cancelled') NOT NULL DEFAULT 'Placed' COMMENT 'placed, preparing, completed, cancelled',
   `payment_mode` varchar(100) NOT NULL COMMENT 'cash,online',
@@ -200,14 +171,17 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `customer_id`, `tracking_no`, `invoice_no`, `total_amount`, `order_date`, `order_status`, `payment_mode`, `order_placed_by_id`) VALUES
-(16, 4, '568089', 'INV-841807', '200', '2024-11-10 01:14:43', 'Completed', 'Cash Payment', 'Kristyle Marie'),
-(17, 5, '460969', 'INV-530521', '200', '2024-11-10 01:15:21', 'Completed', 'Cash Payment', 'Kristyle Marie'),
-(18, 6, '882668', 'INV-464532', '100', '2024-11-10 01:16:35', 'Placed', 'Cash Payment', 'Kristyle Marie'),
-(19, 6, '279301', 'INV-254926', '100', '2024-11-10 01:17:38', 'Preparing', 'Cash Payment', 'Kristyle Marie'),
-(20, 4, '987494', 'INV-382907', '410', '2024-11-10 01:47:03', 'Preparing', 'Cash Payment', 'Kristyle Marie'),
-(21, 6, '000001', 'INV-460557', '100', '2024-11-10 01:57:09', 'Placed', 'Online Payment', 'Kristyle Marie'),
-(22, 6, '000002', 'INV-369317', '100', '2024-11-12 08:56:18', 'Placed', 'Online Payment', 'Kristyle Marie');
+INSERT INTO `orders` (`id`, `customer_id`, `tracking_no`, `invoice_no`, `total_amount`, `amount_received`, `change_money`, `order_date`, `order_status`, `payment_mode`, `order_placed_by_id`) VALUES
+(16, 4, '568089', 'INV-841807', '200', 0.00, '', '2024-11-10 01:14:43', 'Completed', 'Cash Payment', 'Kristyle Marie'),
+(17, 5, '460969', 'INV-530521', '200', 0.00, '', '2024-11-10 01:15:21', 'Completed', 'Cash Payment', 'Kristyle Marie'),
+(18, 6, '882668', 'INV-464532', '100', 0.00, '', '2024-11-10 01:16:35', 'Placed', 'Cash Payment', 'Kristyle Marie'),
+(19, 6, '279301', 'INV-254926', '100', 0.00, '', '2024-11-10 01:17:38', 'Preparing', 'Cash Payment', 'Kristyle Marie'),
+(20, 4, '987494', 'INV-382907', '410', 0.00, '', '2024-11-10 01:47:03', 'Preparing', 'Cash Payment', 'Kristyle Marie'),
+(21, 6, '000001', 'INV-460557', '100', 0.00, '', '2024-11-10 01:57:09', 'Placed', 'Online Payment', 'Kristyle Marie'),
+(22, 6, '000002', 'INV-369317', '100', 0.00, '', '2024-11-12 08:56:18', 'Placed', 'Online Payment', 'Kristyle Marie'),
+(23, 4, '000003', 'INV-557417', '10', 10.00, '0', '2024-11-18 10:15:38', 'Preparing', 'Cash Payment', 'Kristyle Marie'),
+(24, 7, '000004', 'INV-243240', '100', 120.00, '20', '2024-11-18 10:17:31', 'Placed', 'Cash Payment', 'Kristyle Marie'),
+(25, 7, '000005', 'INV-312838', '10', 10.00, '0', '2024-11-18 10:22:21', 'Preparing', 'Cash Payment', 'Kristyle Marie');
 
 -- --------------------------------------------------------
 
@@ -251,7 +225,10 @@ INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `price`, `quantity`) 
 (21, 20, 5, '100.00', '4'),
 (22, 20, 8, '10.00', '1'),
 (23, 21, 5, '100.00', '1'),
-(24, 22, 6, '100.00', '1');
+(24, 22, 6, '100.00', '1'),
+(25, 23, 8, '10.00', '1'),
+(26, 24, 5, '100.00', '1'),
+(27, 25, 8, '10.00', '1');
 
 -- --------------------------------------------------------
 
@@ -275,9 +252,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category_id`, `productname`, `description`, `price`, `image`, `created_at`, `quantity`) VALUES
-(5, 1, 'Pork Sisig (Test 1)', '', 100.00, 'pics/uploads/products/1724471945.jpg\r\n', '2024-11-08', 5),
-(6, 1, 'Tuna Sisig', '', 100.00, 'pics/uploads/products/1724471945.jpg\r\n', '2024-11-08', 9),
-(8, 7, 'Fried Egg', '', 10.00, 'pics/uploads/products/1724471945.jpg\r\n', '2024-11-08', 999);
+(5, 1, 'Pork Sisig (Test 1)', '', 100.00, 'pics/uploads/products/1724471945.jpg\r\n', '2024-11-08', 4),
+(6, 1, 'Tuna Sisig', '', 100.00, 'pics/uploads/products/1724471945.jpg\r\n', '2024-11-08', 0),
+(8, 7, 'Fried Egg', '', 10.00, 'pics/uploads/products/1724471945.jpg\r\n', '2024-11-08', 41);
 
 -- --------------------------------------------------------
 
@@ -303,28 +280,8 @@ CREATE TABLE `purchaseorders` (
 --
 
 INSERT INTO `purchaseorders` (`id`, `customer_id`, `tracking_no`, `invoice_no`, `total_amount`, `order_date`, `order_status`, `ingPayment_mode`, `order_placed_by_id`, `supplierName`) VALUES
-(2, 1, '000002', 'INV-654824', '0', '2024-11-08 20:28:57', 'Placed', 'Cash Payment', 'Admin', '1'),
-(7, 1, '000007', 'INV-225843', '0', '2024-11-08 20:38:24', 'Placed', 'Cash Payment', 'Admin', '2'),
-(10, 1, '000010', 'INV-452490', '600', '2024-11-09 15:10:15', 'Delivered', 'Cash Payment', 'Admin', '2'),
-(11, 1, '000011', 'INV-783935', '120', '2024-11-09 17:11:41', 'Placed', 'Online Payment', 'Admin', '1'),
-(12, 1, '000012', 'INV-856740', '450', '2024-11-09 17:27:12', 'Placed', 'Cash Payment', 'Admin', '2'),
-(13, 1, '000013', 'INV-482793', '900', '2024-11-09 17:30:03', 'Placed', 'Cash Payment', 'Admin', '2'),
-(14, 1, '000014', 'INV-532919', '150', '2024-11-09 17:41:12', 'Placed', 'Online Payment', 'Admin', '2'),
-(15, 1, '000015', 'INV-221178', '150', '2024-11-09 17:42:27', 'Placed', 'Cash Payment', 'Admin', '2'),
-(16, 1, '000016', 'INV-354195', '150', '2024-11-09 17:48:06', 'Cancelled', 'Cash Payment', 'Admin', '2'),
-(17, 1, '000017', 'INV-230437', '590', '2024-11-09 23:29:06', 'Delivered', 'Cash Payment', 'Admin', '2'),
-(18, 1, '000018', 'INV-731227', '70', '2024-11-09 23:38:25', 'Delivered', 'Online Payment', 'Admin', '1'),
-(19, 1, '000019', 'INV-248927', '250', '2024-11-10 02:59:54', 'Delivered', 'Online Payment', 'Admin', '1'),
-(20, 1, '000020', 'INV-577755', '650', '2024-11-10 09:02:58', 'Delivered', 'Online Payment', 'Admin', '1'),
-(21, 1, '000021', 'INV-911298', '940', '2024-11-10 09:16:18', 'Delivered', 'Cash Payment', 'Admin', '2'),
-(22, 1, '000022', 'INV-657691', '330', '2024-11-10 12:14:55', 'Delivered', 'Cash Payment', 'Admin', '1'),
-(23, 1, '000023', 'INV-668421', '10', '2024-11-12 10:15:40', 'Delivered', 'Cash Payment', 'Admin', '1'),
-(24, 1, '000024', 'INV-245198', '390', '2024-11-12 10:18:23', 'Delivered', 'Cash Payment', 'Admin', '1'),
-(25, 1, '000025', 'INV-539462', '250', '2024-11-12 10:19:16', 'Delivered', 'Cash Payment', 'Admin', '1'),
-(26, 1, '000026', 'INV-299256', '150', '2024-11-12 10:22:15', 'Delivered', 'Cash Payment', 'Admin', '2'),
-(27, 1, '000027', 'INV-727538', '480', '2024-11-12 10:23:49', 'Delivered', 'Cash Payment', 'Admin', '1'),
-(28, 1, '000028', 'INV-652270', '450', '2024-11-12 10:26:54', 'Delivered', 'Online Payment', 'Admin', '1'),
-(29, 1, '000029', 'INV-297483', '450', '2024-11-12 10:31:38', 'Delivered', 'Cash Payment', 'Admin', '1');
+(41, 1, '000001', 'INV-653110', '450', '2024-11-18 09:22:23', 'Delivered', 'Cash Payment', 'Kristyle Marie', '1'),
+(42, 1, '000002', 'INV-758307', '1160', '2024-11-18 09:52:41', 'Delivered', 'Online Payment', 'Kristyle Marie', '1');
 
 -- --------------------------------------------------------
 
@@ -411,17 +368,8 @@ CREATE TABLE `stockin` (
 --
 
 INSERT INTO `stockin` (`id`, `admin_id`, `purchaseorder_id`, `invoice_no`, `supplier_id`, `stockin_date`) VALUES
-(1, 2, 21, '0', 2, '2024-11-12 10:12:20'),
-(2, 1, 23, 'INV-668421', 1, '2024-11-12 10:15:53'),
-(3, 1, 23, 'INV-668421', 1, '2024-11-12 10:17:55'),
-(4, 1, 24, 'INV-245198', 1, '2024-11-12 10:18:29'),
-(5, 1, 24, 'INV-245198', 1, '2024-11-12 10:18:31'),
-(6, 1, 25, 'INV-539462', 1, '2024-11-12 10:19:23'),
-(7, 1, 25, 'INV-539462', 1, '2024-11-12 10:19:24'),
-(8, 2, 26, 'INV-299256', 2, '2024-11-12 10:22:24'),
-(9, 2, 26, 'INV-299256', 2, '2024-11-12 10:22:25'),
-(10, 1, 27, 'INV-727538', 1, '2024-11-12 10:23:57'),
-(11, 1, 27, 'INV-727538', 1, '2024-11-12 10:23:59');
+(42, 1, 41, 'INV-653110', 1, '2024-11-18 09:23:30'),
+(43, 1, 42, 'INV-758307', 1, '2024-11-18 09:53:00');
 
 -- --------------------------------------------------------
 
@@ -433,34 +381,25 @@ CREATE TABLE `stockin_ingredients` (
   `id` int(11) NOT NULL,
   `stockin_id` int(11) NOT NULL,
   `ingredient_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
+  `quantity` decimal(10,2) NOT NULL,
   `unit_id` int(11) NOT NULL,
-  `totalPrice` int(11) NOT NULL
+  `totalPrice` int(11) NOT NULL,
+  `expiryDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `stockin_ingredients`
 --
 
-INSERT INTO `stockin_ingredients` (`id`, `stockin_id`, `ingredient_id`, `quantity`, `unit_id`, `totalPrice`) VALUES
-(1, 0, 4, 2, 16, 500),
-(2, 0, 4, 2, 16, 500),
-(3, 0, 4, 1, 16, 250),
-(4, 0, 4, 2, 16, 500),
-(5, 0, 4, 2, 16, 500),
-(6, 0, 4, 2, 16, 500),
-(7, 0, 5, 1, 14, 10),
-(8, 0, 6, 1, 19, 70),
-(9, 1, 4, 4, 16, 600),
-(10, 1, 7, 2, 16, 340),
-(11, 2, 5, 1, 14, 10),
-(12, 3, 5, 1, 14, 10),
-(13, 4, 4, 1, 16, 250),
-(14, 4, 6, 2, 19, 140),
-(15, 5, 4, 1, 16, 250),
-(16, 5, 6, 2, 19, 140),
-(17, 6, 4, 1, 16, 250),
-(18, 7, 4, 1, 16, 250);
+INSERT INTO `stockin_ingredients` (`id`, `stockin_id`, `ingredient_id`, `quantity`, `unit_id`, `totalPrice`, `expiryDate`) VALUES
+(72, 42, 5, 2.00, 14, 20, '2024-12-07'),
+(73, 42, 6, 2.00, 19, 140, '2024-11-18'),
+(74, 42, 4, 1.00, 16, 250, '2024-11-18'),
+(75, 42, 8, 1.00, 15, 120, '2024-11-28'),
+(76, 43, 5, 2.00, 14, 20, '2024-11-29'),
+(77, 43, 6, 4.00, 19, 280, '2024-12-04'),
+(78, 43, 4, 2.00, 16, 500, '2024-11-30'),
+(79, 43, 8, 3.00, 15, 360, '2024-12-05');
 
 -- --------------------------------------------------------
 
@@ -534,8 +473,8 @@ CREATE TABLE `units_of_measure` (
 
 INSERT INTO `units_of_measure` (`id`, `category_id`, `uom_name`, `type`, `ratio`, `active`, `rounding_precision`) VALUES
 (13, 5, 'g', 'reference', 1.00000, 1, 0.01000),
-(14, 6, 'Pieces', 'reference', 1.00000, 1, 0.01000),
-(15, 6, 'Dozen', 'bigger', 12.00000, 1, 0.01000),
+(14, 6, 'pcs', 'reference', 1.00000, 1, 0.01000),
+(15, 6, 'dozen', 'bigger', 12.00000, 1, 0.01000),
 (16, 5, 'kg', 'bigger', 1000.00000, 1, 0.01000),
 (18, 7, 'ml', 'reference', 1.00000, 1, 0.01000),
 (19, 7, 'L', 'bigger', 1000.00000, 1, 0.01000),
@@ -697,7 +636,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `ingredients`
@@ -709,19 +648,19 @@ ALTER TABLE `ingredients`
 -- AUTO_INCREMENT for table `ingredients_items`
 --
 ALTER TABLE `ingredients_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -733,7 +672,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `purchaseorders`
 --
 ALTER TABLE `purchaseorders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `recipes`
@@ -751,13 +690,13 @@ ALTER TABLE `recipe_ingredients`
 -- AUTO_INCREMENT for table `stockin`
 --
 ALTER TABLE `stockin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `stockin_ingredients`
 --
 ALTER TABLE `stockin_ingredients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
