6f527441010ec30e8e041fba2b289242d250c220
smm23 <shilohm.millondaga@gmail.com>
2025-04-26 14:24:30 +0800

    Fetch only active suppliers

--- admin/purchase-order-select-supplier.php
+++ admin/purchase-order-select-supplier.php
@@ -13,27 +13,30 @@ unset($_SESSION['ingredientItemIds']);
             <h4 class="mb-0">Create Purchase Order</h4>
         </div>
         <div class="card-body">
-        <?php alertMessage(); ?>
+            <?php alertMessage(); ?>
             <form action="purchase-orders-code.php" method="POST">
                 <div class="row">
                     <div class="col-md-4">
-                        <label for="">Select Supplier</label>
+                        <label for="supplierName">Select Supplier</label>
                         <select name="supplier_id" id="supplierName" class="form-select mySelect2">
                             <option value="">-- Select Supplier --</option>
                             <?php
-                                $suppliers = getAll('suppliers');
-                                if($suppliers) {
-                                    if(mysqli_num_rows($suppliers) > 0) {
-                                        foreach($suppliers as $supplierItems) {
-                                            ?>
-                                                <option value="<?= $supplierItems['id']; ?>"><?= $supplierItems['firstname']; ?></option>
-                                            <?php
-                                        }
-                                    } else {
-                                        echo '<option value="">No Suppliers found!</option>';
+                                // Fetch only active suppliers
+                                $query = "SELECT id, firstname, lastname 
+                                          FROM suppliers 
+                                          WHERE status = 'active'";
+                                $suppliers = mysqli_query($conn, $query);
+
+                                if ($suppliers && mysqli_num_rows($suppliers) > 0) {
+                                    while ($supplierItems = mysqli_fetch_assoc($suppliers)) {
+                                        ?>
+                                        <option value="<?= $supplierItems['id']; ?>">
+                                            <?= $supplierItems['firstname'] . ' ' . $supplierItems['lastname']; ?>
+                                        </option>
+                                        <?php
                                     }
                                 } else {
-                                    echo '<option value="">Something went wrong!</option>';
+                                    echo '<option value="">No Active Suppliers found!</option>';
                                 }
                             ?>
                         </select>
